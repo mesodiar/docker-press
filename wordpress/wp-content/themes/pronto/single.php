843440687c82e007d00f423a29bde933bcc5f312
Mils <mils@prontomarketing.com>
2018-04-04 14:40:56 +0700

    Using hook

--- wordpress/wp-content/themes/pronto/single.php
+++ wordpress/wp-content/themes/pronto/single.php
@@ -1,6 +1,6 @@
 <h1> THis is single.php </h1>
 <?php
-    global $wp_query;
+    //global $wp_query;
 
     // echo 'is_singular';
     // var_dump($wp_query->is_singular);
@@ -16,13 +16,31 @@
     // echo 'is_home';
     // var_dump($wp_query->is_home);
 
-    if ( have_posts() ):
-        while( have_posts() ): the_post();
-            echo the_title( $before='<h1>', $after='</h1>' );
-            echo '<p>' . the_content() . '</p>';
-    endwhile;
-    endif;
+    // if ( have_posts() ):
+    //     while( have_posts() ): the_post();
+    //         echo the_title( $before='<h1>', $after='</h1>' );
+    //         echo '<p>' . the_content() . '</p>';
+    // endwhile;
+    // endif;
+    function uppercase( $content ) {
+        return strtoupper( $content);
+    }
+    add_filter('the_content', 'uppercase');
+    //echo uppercase('kan')
+
+    function concat( $x, $y, $z ) {
+        return $x . $y . $z;
+    }
+
+    add_filter('pronto', 'concat', 10, 3);
+
+
+    $name= 'Under';
+    $position = 'Apple';
+    $result = apply_filters( 'pronto', $result, $name, $position );
+    var_dump( $result );
+
 
     the_post();
-    echo the_title( $before='<h1>', $after='</h1>' );
-    echo '<p>' . the_content() . '</p>';
+        echo the_title( $before='<h1>', $after='</h1>' );
+        echo '<p>' . the_content() . '</p>';
