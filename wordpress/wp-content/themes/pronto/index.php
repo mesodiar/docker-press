c3148934106021396c660c80ff625f1706cf661d
Mils <mils@prontomarketing.com>
2018-04-04 14:18:33 +0700

    Using wordpress functions :koala::apple:

--- wordpress/wp-content/themes/pronto/index.php
+++ wordpress/wp-content/themes/pronto/index.php
@@ -1,18 +1,29 @@
 <h1> Heelo, Under</h1>
 <?php
-    global $wp_query;
+    // global $wp_query;
 
-    echo 'is_singular';
-    var_dump($wp_query->is_singular);
-    var_dump( is_singular() );
+    // echo 'is_singular';
+    // var_dump($wp_query->is_singular);
+    // var_dump( is_singular() );
 
-    echo 'is_page';
-    var_dump($wp_query->is_page);
+    // echo 'is_page';
+    // var_dump($wp_query->is_page);
 
-    echo 'is_single';
-    var_dump($wp_query->is_single);
+    // echo 'is_single';
+    // var_dump($wp_query->is_single);
 
-    echo 'is_home';
-    var_dump($wp_query->is_home);
-
-    var_dump( have_posts() );
+    // echo 'is_home';
+    // var_dump($wp_query->is_home);
+?>
+<div>
+    <?php
+        if ( have_posts() ):
+            while( have_posts() ): the_post(); //we can use this because we already define global variable
+                echo '<h2>' . the_title() .'</h2>';
+                echo the_title($before='<h1>', $after='</h1>');
+                echo the_content($before='<h2>', $after='</h2>');
+                //echo '<h2>' . the_content() . '</h2>';
+            endwhile;
+        endif;
+    ?>
+</div>
--- wordpress/wp-content/themes/pronto/single.php
+++ wordpress/wp-content/themes/pronto/single.php
@@ -2,16 +2,27 @@
 <?php
     global $wp_query;
 
-    echo 'is_singular';
-    var_dump($wp_query->is_singular);
-    var_dump( is_singular() );
+    // echo 'is_singular';
+    // var_dump($wp_query->is_singular);
+    // var_dump( is_singular() );
 
-    echo 'is_page';
-    var_dump($wp_query->is_page);
-    var_dump( is_page() );
+    // echo 'is_page';
+    // var_dump($wp_query->is_page);
+    // var_dump( is_page() );
 
-    echo 'is_single';
-    var_dump($wp_query->is_single);
+    // echo 'is_single';
+    // var_dump($wp_query->is_single);
 
-    echo 'is_home';
-    var_dump($wp_query->is_home);
+    // echo 'is_home';
+    // var_dump($wp_query->is_home);
+
+    if ( have_posts() ):
+        while( have_posts() ): the_post();
+            echo the_title( $before='<h1>', $after='</h1>' );
+            echo '<p>' . the_content() . '</p>';
+    endwhile;
+    endif;
+
+    the_post();
+    echo the_title( $before='<h1>', $after='</h1>' );
+    echo '<p>' . the_content() . '</p>';
